e5a6839b1f27c1f1c8fffec89a08107341bbb3f4
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2021-11-10 01:33:05 +0100

    add more test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,5 +1,9 @@
-import { greet } from "./main";
+import { greet, upper } from "./main";
 
 test("greeting", () => {
     expect(greet("Foo")).toBe("Hello, Foo!");
 });
+
+test("upper case", () => {
+    expect(upper("Hello :)")).toBe("HELLO :)")
+})
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 export function greet(name: string): string {
     return `Hello, ${name}!`;
 }
+
+export function upper(input: string): string {
+    return input.toUpperCase();
+}
